6051009834ea7a28870c531545af28ba20b39b2e
Oliver Siwers <20802536+OliverSiwers@users.noreply.github.com>
2023-08-27 15:43:20 +0200

    Add files via upload

---
+++ SimpleServer.bat
@@ -0,0 +1,3 @@
+@echo off
+node --eval="const http = require('http');const fs = require('fs/promises');const server = http.createServer(async (req, res) => {    console.log(req.url);    let path = req.url;    if (path.endsWith('/')) path += 'index.html';    try {        res.end(await fs.readFile('.' + path));    } catch {        res.writeHead(404);        res.end('404');    }}).listen(80, 'localhost', () => {    console.log(server.address());});"
+pause
